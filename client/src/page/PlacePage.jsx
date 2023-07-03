d959133355530346a03333df662e8d7b82119687
johnie261 <njorogejohn261@gmail.com>
2023-07-03 14:33:47 +0300

    Add upload photos and perks functunality

--- client/src/page/PlacePage.jsx
+++ client/src/page/PlacePage.jsx
@@ -27,12 +27,14 @@ const PlacePage = () => {
   const uploadPhoto = (e) => {
     const files = e.target.files
     const data = new FormData()
-    data.set('photos', files)
-    axios.post('/uploads', data, {
+    for(let i = 0; i<files.length; i++) {
+        data.set('photos', files[i])
+    }
+    axios.post('/upload', data, {
       headers: {'content-type' : 'multipart/form-data'}
     }).then(res => {
-      const {data:filename} = res
-      setAddedPhotos((prev) => [...prev, filename]);
+      const {data:filenames} = res
+      setAddedPhotos((prev) => [...prev, ...filenames]);
     })
   }
 
@@ -74,12 +76,12 @@ const PlacePage = () => {
             </div>
             <div className='mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6'> 
               {addedPhotos.length > 0 && addedPhotos.map((link, index) => (
-                <div key={index}>
-                  <img className='rounded-2xl' src={'http://localhost:4000/uploads/'+link} />
+                <div key={index} className='flex h-32'>
+                  <img className='rounded-2xl w-full object-cover' src={'http://localhost:4000/uploads/'+link} />
                 </div>
                ))}
-              <label className='flex justify-center items-center gap-1 bg-transparent border rounded-2xl p-2 text-2xl text-gray-600'>
-                <input type='file' onChange={uploadPhoto} className='hidden'/>
+              <label className='h-32 cursor-pointer flex justify-center items-center gap-1 bg-transparent border rounded-2xl p-2 text-2xl text-gray-600'>
+                <input multiple type='file' onChange={uploadPhoto} className='hidden'/>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                 </svg>
--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const download = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
+
 require('dotenv').config();
 
 //app config
@@ -95,5 +98,18 @@ app.post('/upload-by-link', async(req, res) => {
   res.json(newName)
 })
 
+const photosMiddleware = multer({dest: 'uploads/'})
+app.post('/upload', photosMiddleware.array('photos', 100), (req,res) => {
+  const uploadedFiles = []
+  for(let i = 0; i < req.files.length; i++) {
+    const {path, originalname} = req.files[i]
+    const parts = originalname.split('.')
+    const ext = parts[parts.length-1]
+    const newPath = path + '.' + ext
+    fs.renameSync(path, newPath)
+    uploadedFiles.push(newPath.replace('uploads', ''))
+  }
+  res.json(uploadedFiles)
+})
 //listener
 app.listen(4000)
